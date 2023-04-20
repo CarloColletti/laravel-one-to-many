bd07d3351c2b99b9dba266062974d5a71c9f664a
CarloColletti~ <carlocolletti5@gmail.com>
2023-04-20 15:10:20 +0200

    inserite ultime cose nello show

--- resources/views/admin/Shoe/show.blade.php
+++ resources/views/admin/Shoe/show.blade.php
@@ -6,18 +6,13 @@
 
 @section('content')
   <div class="container">
-    {{-- botton return to shoe list  --}}
-    <div class="row">
-      <a href="Admin.Shoe.index" class="btn ms-auto">Torna alla lista</a>
-    </div>
-
     {{-- detail shoe  --}}
     <div class="row">
       <h2>
         Stai visualizzando la Scarpa: {{$Shoe->name}}
       </h2>
       {{-- list detail  --}}
-      <div class="col-5">
+      <div class="col-7">
         <ul>
           <li>Nome: {{$Shoe->name}}</li>
           <li>Brand: {{$Shoe->brand}}</li>
@@ -26,6 +21,19 @@
           <li>Tipologia: {{$Shoe->type}}</li>
         </ul>
       </div>
+
+      {{-- image shoe  --}}
+      <div class="col-5">
+        <img src="{{ $Shoe->img }}" alt="">
+      </div>
     </div>
+
+    {{-- botton return to shoe list  --}}
+    <div class="row py-5">
+      <div class="col-2 ms-auto">
+        <a href="{{ route('Admin.Shoe.index') }}" class="btn btn-outline-success">Torna alla lista</a>
+      </div>
+    </div>
+
   </div>
 @endsection
